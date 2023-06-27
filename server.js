bdf31eddaa53e70c90e8e57bc8d26877f3b4cab7
Akshay Sutar <akshay.sutar5@outlook.com>
2023-06-27 14:39:02 +0530

    minor change

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ app.use(express.json());
 app.use(cors());
 
 app.get("/", (req, res) => {
-  return res.json(config);
+  return res.json("Hello");
 });
 
 app.post("/board", boardController.createBoard);
